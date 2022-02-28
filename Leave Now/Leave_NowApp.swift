4d8ea36db6aadadb01dd80ac247f028a03c60efa
Zsolt Kébel <kebel.zsolt@gmail.com>
2022-02-28 16:27:10 +0000

    Initial Commit

---
+++ Leave Now/Leave_NowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leave_NowApp.swift
+//  Leave Now
+//
+//  Created by Zsolt Kébel on 28/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Leave_NowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Leave NowUITests/Leave_NowUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Leave_NowUITestsLaunchTests.swift
+//  Leave NowUITests
+//
+//  Created by Zsolt Kébel on 28/02/2022.
+//
+
+import XCTest
+
+class Leave_NowUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
